aa666315bc55c06bf6f7da62e2639d793524a3bf
Syed M Muslim <117216018+Syed-Muslim-19@users.noreply.github.com>
2023-06-29 01:50:50 +0500

    Update portfolio.js

--- MyPortfolio/portfolio.js
+++ MyPortfolio/portfolio.js
@@ -1,4 +1,13 @@
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
-$('#skill').tooltip('show')
+document.addEventListener('DOMContentLoaded', function() {
+  // Get all the buttons inside the cards
+  var buttons = document.querySelectorAll('.card-body .btn');
+
+  // Add click event listener to each button
+  buttons.forEach(function(button) {
+    button.addEventListener('click', function(event) {
+      event.preventDefault(); // Prevent the default link behavior
+      var url = this.getAttribute('href'); // Get the URL from the href attribute
+      window.location.href = url; // Navigate to the URL in the same page
+    });
+  });
+});
